699bed6024a2ec0a6b77fc22cadd2fff256a06df
Allison Harris <allison.harris23@gmail.com>
2014-12-16 18:16:58 -0600

    Continuing to fix javascript errors

--- calculator.js
+++ calculator.js
@@ -45,8 +45,18 @@ for(var i = 0; i < keys.length; i++) {
 					//Only add operator if input is not empty and there is no operator at the last
 					if(inputVal != '' && operators.indexOf(lastChar) == -1) {
 						input.innerHTML += btnVal;
+
+					//Allow minus if the string is empty
+					else if(inputVal == '' && btnVal == '-')
+						input.innerHTML += btnVal;
 					}
-				}
+					
+					//Replace the last operator (if exists) with the newly pressed operator
+						if(operators.indexOf(lastChar) > -1 {
+							//Here, '.' matches any character while $ donotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
+								input.innerHTML = inputVal.replace(/.$/, btnVal);
+							}
+						}
 			//if any other key is pressed, just append input
 			else {
 				input.innerHTML += btnVal;
