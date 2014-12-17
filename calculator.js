bbb638c1b2710ac6a66f1a9c02bbca23a8a511c0
Allison Harris <allison.harris23@gmail.com>
2014-12-16 18:18:27 -0600

    Added inputVal length for the last operator

--- calculator.js
+++ calculator.js
@@ -52,7 +52,7 @@ for(var i = 0; i < keys.length; i++) {
 					}
 					
 					//Replace the last operator (if exists) with the newly pressed operator
-						if(operators.indexOf(lastChar) > -1 {
+						if(operators.indexOf(lastChar) > -1 && inputVal.length > 1 {
 							//Here, '.' matches any character while $ donotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 								input.innerHTML = inputVal.replace(/.$/, btnVal);
 							}
