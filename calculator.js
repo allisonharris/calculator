f36819aea732d435c12473b052b6e326329dab45
Allison Harris <allison.harris23@gmail.com>
2015-03-05 08:30:21 -0600

    added missing parenthesis
    functionality is still not working entirely correctly

--- calculator.js
+++ calculator.js
@@ -58,7 +58,7 @@ for(var i = 0; i < keys.length; i++) {
 						input.innerHTML += btnVal;
 					
 					//Replace the last operator (if exists) with the newly pressed operator
-						if(operators.indexOf(lastChar) > -1 && inputVal.length > 1 {
+						if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
 							//Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
 								input.innerHTML = inputVal.replace(/.$/, btnVal);
 
